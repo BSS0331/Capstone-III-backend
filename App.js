6dbc34357ed038ee448ca0183708e7a61dfb4bd2
Seop <bsseop0331@naver.com>
2024-04-01 03:18:33 +0900

    테에스으트으

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ function HomeScreen({ navigation }) {
       <FAB.Group  // 플로팅 액션 버튼
         open={open}
         icon={open ? 'close' : 'plus'}
-        actions={[  // 액션 아이콘
+        actions={[  // 액션 아이콘 정의
           { icon: 'cog', label: '설정', onPress: () => console.log('설정 Pressed'), },
           { icon: 'pencil', label: '수동 입력', onPress: () => console.log('수동 입력 Pressed'), },
           { icon: 'receipt', label: '영수증', onPress: () => console.log('영수증 Pressed'), },
